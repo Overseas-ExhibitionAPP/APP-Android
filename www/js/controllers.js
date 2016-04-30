f15d4fc24934f179b9fec70c465cea1d166729ff
Jeff <euug1782@gmail.com>
2016-05-01 01:03:00 +0800

    修改活動花絮以幻燈片支方式呈現

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -47,5 +47,61 @@ angular.module('starter.controllers', ['starter.services','ui.bootstrap','ngAnim
     $scope.alphabet = ['NCNU_LOGO.png','Block.png','Block.png','Block.png','Block.png','Block.png','Block.png','Block.png','Block.png','Block.png'];
 
 })
+.controller('CarouselDemoCtrl', function ($scope) {
+  $scope.myInterval = 2000;
+  $scope.noWrapSlides = false;
+  $scope.active = 0;
+  var slides = $scope.slides = [];
+  var currIndex = 0;
 
+  $scope.addSlide = function() {
+    var newWidth = 500 + slides.length + 1;
+    slides.push({
+      image: '/img/photos/1.jpg',
+      text: ['Nice image','Awesome photograph','That is so cool'][slides.length % 3],
+      id: currIndex++
+    });
+  };
+
+  $scope.randomize = function() {
+    var indexes = generateIndexesArray();
+    assignNewIndexesToSlides(indexes);
+  };
+
+  for (var i = 0; i < 4; i++) {
+    $scope.addSlide();
+  }
+
+  // Randomize logic below
+
+  function assignNewIndexesToSlides(indexes) {
+    for (var i = 0, l = slides.length; i < l; i++) {
+      slides[i].id = indexes.pop();
+    }
+  }
+
+  function generateIndexesArray() {
+    var indexes = [];
+    for (var i = 0; i < currIndex; ++i) {
+      indexes[i] = i;
+    }
+    return shuffle(indexes);
+  }
+
+  // http://stackoverflow.com/questions/962802#962890
+  function shuffle(array) {
+    var tmp, current, top = array.length;
+
+    if (top) {
+      while (--top) {
+        current = Math.floor(Math.random() * (top + 1));
+        tmp = array[current];
+        array[current] = array[top];
+        array[top] = tmp;
+      }
+    }
+
+    return array;
+  }
+})
 ;
